9bcd5c047e9c42c0aabc4328f5d3cc8cfb888c83
Tomoya Tabuchi <t@tomoyat1.com>
2016-09-05 11:17:32 +0900

    Remove magic numbers and fix style

--- draw.lua
+++ draw.lua
@@ -1,24 +1,55 @@
 require 'cairo'
 
+--config
+-------------------------------------------------------------------------------
+base = { 
+    x = 440,
+    y = 0,
+    font = "M+ 1mn thin",
+    color = {
+        r = 0.52734375,
+        g = 0.68359375,
+        b = 1,
+        a = 1,
+    },
+    monitor = {},
+    clock = {},
+}
+
+base.monitor = {
+    x = 510,
+    y = 320,
+    radius = 250,
+    interval = 25,
+
+}
+
+base.clock = {
+    x = 10,
+    y = 140,
+    time_font = "M+ 1mn thin",
+    time_font_size = 169,
+    date_font_size = 36
+}
+
+--main draw function
+-------------------------------------------------------------------------------
 function conky_main()
     if conky_window == nil then
         return
     end
     local cs = cairo_xlib_surface_create(conky_window.display,
-                                         conky_window.drawable,
-                                         conky_window.visual,
-                                         conky_window.height,
-                                         conky_window.width)
+        conky_window.drawable, conky_window.visual, conky_window.height,
+        conky_window.width)
     local cr = cairo_create(cs)
-    -- I know, origin is bogus
-    x_base = 700
-    y_base = -30
 
-    cairo_select_font_face(cr, "M+ 1mn thin", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-    cairo_set_source_rgba (cr,0.52734375,0.68359375,1,1)
+    cairo_select_font_face(cr, base.font, CAIRO_FONT_SLANT_NORMAL,
+        CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_set_source_rgba(cr,base.color.r, base.color.g, base.color.b,
+        base.color.a)
 
-    cairo_set_line_width (cr,5)
-    cairo_set_source_rgba (cr,0.52734375,0.68359375,1,1)
+    cairo_set_line_width(cr,5)
+    cairo_set_source_rgba(cr,0.52734375,0.68359375,1,1)
 
     cpu_perc=tonumber(conky_parse("${cpu}"))
     mem_perc=tonumber(conky_parse("${memperc}"))
@@ -27,36 +58,45 @@ function conky_main()
     if cpu_perc > 90 then
         cairo_set_source_rgba (cr,1,0.46875,0.68359375,1)
     end
-    cairo_arc (cr, x_base + 250, y_base + 350, 250, - math.pi/2, - math.pi/2 + 2.7 * cpu_perc * (math.pi/180))
+    cairo_arc(cr, base.x + base.monitor.x, base.y + base.monitor.y,
+        base.monitor.radius, - math.pi/2,
+        - math.pi/2 + 2.7 * cpu_perc * (math.pi/180))
     cairo_stroke(cr)
 
     cairo_set_source_rgba (cr,0.52734375,0.68359375,1,1)
     if mem_perc > 87.5 then
         cairo_set_source_rgba (cr,1,0.46875,0.68359375,1)
     end
-    cairo_arc (cr, x_base + 250, y_base + 350, 275, - math.pi/2, - math.pi/2 + 2.7 * mem_perc * (math.pi/180))
+    cairo_arc(cr, base.x + base.monitor.x, base.y + base.monitor.y,
+        base.monitor.radius + base.monitor.interval, - math.pi/2,
+        - math.pi/2 + 2.7 * mem_perc * (math.pi/180))
     cairo_stroke(cr)
 
     cairo_set_source_rgba (cr,0.52734375,0.68359375,1,1)
     if bat_perc < 20 then
         cairo_set_source_rgba (cr,1,0.46875,0.68359375,1)
     end
-    --cairo_arc (cr, x_base + 250, y_base + 350, 300, - math.pi/2, - math.pi/2 + 2.7 * 100 * (math.pi/180))
-    cairo_arc (cr, x_base + 250, y_base + 350, 300, - math.pi/2, - math.pi/2 + 2.7 * bat_perc * (math.pi/180))
+    --[[cairo_arc(cr, base.x + base.monitor.x, base.y + base.monitor.y,
+        base.monitor.radius + (base.monitor.interval * 2), - math.pi/2,
+        - math.pi/2 + 2.7 * bat_perc * (math.pi/180))--]]
+    cairo_arc(cr, base.x + base.monitor.x, base.y + base.monitor.y,
+        base.monitor.radius + (base.monitor.interval * 2), - math.pi/2,
+        - math.pi/2 + 2.7 * bat_perc * (math.pi/180))
     cairo_stroke(cr)
 
-    x_clock_base = -250
-    y_clock_base = 170
-    cairo_set_source_rgba (cr,0.52734375,0.68359375,1,1)
-    cairo_select_font_face(cr, "M+ 1mn light", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-    cairo_move_to(cr, x_base + x_clock_base, y_base + y_clock_base);
-    cairo_set_font_size(cr, 36)
+    cairo_set_source_rgba( cr,base.color.r, base.color.g, base.color.b,
+        base.color.a)
+    cairo_select_font_face(cr, base.font, CAIRO_FONT_SLANT_NORMAL,
+        CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_move_to(cr, base.x + base.clock.x, base.y + base.clock.y);
+    cairo_set_font_size(cr, base.clock.date_font_size)
     cairo_show_text(cr, conky_parse("${time %a %b %d}"));
     cairo_stroke(cr)
 
-    cairo_select_font_face(cr, "M+ 1mn thin", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-    cairo_move_to(cr, x_base + x_clock_base - 13, y_base + y_clock_base + 140)
-    cairo_set_font_size(cr, 169)
+    cairo_select_font_face(cr, base.clock.time_font, CAIRO_FONT_SLANT_NORMAL,
+        CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_move_to(cr, base.x + base.clock.x - 13, base.y + base.clock.y + 140)
+    cairo_set_font_size(cr, base.clock.time_font_size)
     cairo_show_text(cr, conky_parse("${time %H:%M}"));
     cairo_stroke(cr)
 
